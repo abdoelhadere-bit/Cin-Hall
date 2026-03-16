6b32192189a95b0907d8dbdde4947b7da44bb8fc
mo24ali <alichere1234@gmail.com>
2026-03-16 14:30:22 +0000

    payment process

--- database/migrations/2026_03_16_134131_create_payment_table.php
+++ database/migrations/2026_03_16_134131_create_payment_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('payment', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('reservation_id')->constrained('reservations')->cascadeOnDelete();
+            $table->string('payment_method');
+            $table->string('transaction_id')->nullable();
+            $table->decimal('amount',8,2);
+            $table->string('status');
             $table->timestamps();
         });
     }
